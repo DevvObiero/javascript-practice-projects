cd7e71bd0225e582727701b55eb2f18dc4e44f6c
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2024-02-22 13:59:12 +0300

    first commit

---
+++ assignments.js
@@ -0,0 +1,32 @@
+/*
+let country="Kenya";
+const continent="Africa";
+let population="54.03 million";
+
+
+
+const isIsland= false;
+let language="Kiswahili";
+console.log(language);
+console.log(isIsland);
+console.log(country);
+console.log(population);
+console.log(continent);
+
+continent= Tanzania;*/
+
+const country="Kenya";
+let population= 54300000;
+let finlandPopulation = 6000000;
+const avgPopulation = 33000000;
+const description ='Portugal is in Europe, and its 11 million people speak portuguese'
+
+
+
+console.log(population/2);
+population++;
+console.log(population);
+console.log(population>finlandPopulation);
+console.log(population<avgPopulation);
+console.log(description);
+
---
+++ script.js
@@ -0,0 +1,77 @@
+/*let js ="amazing";
+ 
+console.log("jonas");
+console.log(1 + 2 + 56);
+
+let firstName="Matilda";
+let first="omondi";
+console.log(firstName);*/
+
+/*true;
+console.log(true);
+let javascriptIsFun="true";
+console.log(javascriptIsFun);
+console.log( typeof true)
+console.log(typeof "Obiero");
+
+javascriptIsFun= "yes";
+
+console.log(  javascriptIsFun);
+
+let year;
+console.log(year);
+
+year= new Date
+console.log(year);
+console.log(typeof null)
+*/
+
+/*  let age=18
+age=19
+console.log(age);
+
+const birthyear=2005;
+birthyear=2004;
+*/
+//console.log(birthyear);
+/*var should be contanlty avoides */    
+/*
+var js = "amazing";
+js="not amazing";*/
+
+
+
+//math operatores 
+
+const now= 2037;
+const agePaul = now - 2005;
+const ageWife = now - 2008;
+console.log(agePaul, ageWife);
+
+console.log(agePaul*2,ageWife/10,2**3);
+//2**3 just means 2 pwer 3
+
+const firstName= "Paul";
+const lastName= "Obiero";
+
+console.log(firstName + " " + lastName);
+
+
+//asignment opperators
+let x= 10 + 9;
+x+=10;//x is equal to x +10
+x*=10;
+x++;
+x--;
+console.log(x);
+
+//comparison oerators
+
+console.log(agePaul>ageWife);
+console.log(ageWife>=18);
+
+const isFullAge = ageWife >= 18;
+
+console.log(now-1991>now-1888);
+
+console.log(25-10-5);
