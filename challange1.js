1982f968d95d0075cd0019a84694607616926319
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2024-02-29 08:19:23 +0300

    challange 3

--- challange1.js
+++ challange1.js
@@ -18,4 +18,29 @@ const markHigherBMI=BMIMark>BMIJohn;
 
 if(markHigherBMI){
     console.log(`"Mark's BMI ${BMIMark} is higher than John's ${BMIJohn}"`)}*/
+// const scoreDolphins=(96+108+89)/3;
+// const scoreKoalas=(88+91+110)/3;
+// console.log(scoreDolphins,scoreKoalas)
 
+// if(scoreDolphins>scoreKoalas){console.log('Dolphins win!!🐱‍👤')
+// }else if( scoreKoalas>scoreDolphins){
+//      console.log('Koalas win!!🐨')
+// }else if(scoreDolphins===scoreKoalas){
+//      console.log('Its a tie🦥')
+// }
+
+//BONUS !
+const scoreDolphins=(97+112+80)/3;
+const scoreKoalas=(109+95+50)/3;
+
+
+console.log(scoreDolphins,scoreKoalas)
+
+if(scoreDolphins>scoreKoalas&&scoreDolphins>=100){console.log('Dolphins win!!🐱‍👤')
+}else if( scoreKoalas>scoreDolphins&&scoreKoalas>=100){
+     console.log('Koalas win!!🐨')
+}else if(scoreDolphins===scoreKoalas&&scoreDolphins>=100&&scoreKoalas>=100)
+    {console.log('Its a tie🦥')
+}else{
+     console.log('No one wins the trophy😫(Score of 100 not meet by any)')
+}
--- script.js
+++ script.js
@@ -207,29 +207,3 @@ if(favourite!==25){
 // }else{console.log('You should not drive')
 // }
 
-// const scoreDolphins=(96+108+89)/3;
-// const scoreKoalas=(88+91+110)/3;
-// console.log(scoreDolphins,scoreKoalas)
-
-// if(scoreDolphins>scoreKoalas){console.log('Dolphins win!!🐱‍👤')
-// }else if( scoreKoalas>scoreDolphins){
-//      console.log('Koalas win!!🐨')
-// }else if(scoreDolphins===scoreKoalas){
-//      console.log('Its a tie🦥')
-// }
-
-//BONUS !
-const scoreDolphins=(97+112+80)/3;
-const scoreKoalas=(109+95+50)/3;
-
-
-console.log(scoreDolphins,scoreKoalas)
-
-if(scoreDolphins>scoreKoalas&&scoreDolphins>=100){console.log('Dolphins win!!🐱‍👤')
-}else if( scoreKoalas>scoreDolphins&&scoreKoalas>=100){
-     console.log('Koalas win!!🐨')
-}else if(scoreDolphins===scoreKoalas&&scoreDolphins>=100&&scoreKoalas>=100)
-    {console.log('Its a tie🦥')
-}else{
-     console.log('No one wins the trophy😫(Score of 100 not meet by any)')
-}
